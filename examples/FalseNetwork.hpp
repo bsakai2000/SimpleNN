f58fbe7d7cdf321b0ba8b8702473ba99d6fe2c15
Brandon Sakai <brandon.sakai@gmail.com>
2020-02-17 21:46:17 -0500

    Added additional comments

--- examples/FalseNetwork.hpp
+++ examples/FalseNetwork.hpp
@@ -8,6 +8,8 @@ class Network
 	public:
 		Network(int num_inputs, int num_layers, int nodes_per_layer, int num_outputs)
 		{
+			// Read the RGB file to get the actual values of the pixels so
+			// we can send them to the other code
 			FILE* image_file = fopen(filename, "rb");
 
 			if(image_file == NULL)
@@ -16,21 +18,24 @@ class Network
 				exit(1);
 			}
 
-			// Read the BMP header to get necessary information
+			// Read the image file
 			image = (unsigned char*) malloc(height * width * 3 * sizeof(unsigned char));
 			fread(image, sizeof(unsigned char), height * width * 3, image_file);
 		}
 
 		~Network()
 		{
+			free(image);
 			return;
 		}
 
 		double* forward_propagate(double* input)
 		{
+			// Find the location in the array where that pixel is
 			double* pixel = (double*) malloc(3 * sizeof(double));
 			unsigned char* pixel_ptr = image + (int) ((input[0] + (height - 1 - input[1]) * width) * 3);
 
+			// Load those values into the pixel and return them
 			for(int i = 0; i < 3; ++i)
 			{
 				pixel[i] = (double) pixel_ptr[2 - i];
@@ -38,6 +43,7 @@ class Network
 			return pixel;
 		}
 
+		// No need to change anything when training, we just need to avoid linker errors
 		void train(double** inputs, double** expected_outputs, int num_inputs)
 		{
 			return;
